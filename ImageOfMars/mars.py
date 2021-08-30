5012066b3ad6d40639c8ae9c74edc21055912dca
btan-fluxus <56370618+btan-fluxus@users.noreply.github.com>
2021-08-29 18:30:44 -0700

    generated png correctly

--- ImageOfMars/mars.py
+++ ImageOfMars/mars.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-import copy
 
 
 mars_gray = mpimg.imread('mars.png') # Shape (501, 1024, 3)
@@ -11,17 +10,14 @@ lum_img = mars_gray[:, :, 0]
 # print(mars_gray.shape)
 # print(lum_img.shape)
 
-mars_colormap = {'red':     [ [0.0, 0.0, 0.0], [0.2, 0.0, 0.14], [0.75, 1.0, 1.0], [1.0, 0.7, 0.7] ],
-                 'green':   [ [0.0, 0.0, 0.14], [0.24, 0.14, 0.24], [0.3, 0.7, 0.7], [0.4, 0.3, 0.0], [1.0, 0.0, 0.0] ],
-                 'blue':    [ [0.0, 1.0, 1.0], [0.28, 0.0, 0.0], [1.0, 0.0, 0.0] ]}
+mars_colormap_dict =    {   'red':     [ [0.0, 0.0, 0.0], [0.2, 0.0, 0.14], [0.75, 1.0, 1.0], [1.0, 0.7, 0.7] ],
+                            'green':   [ [0.0, 0.0, 0.14], [0.24, 0.14, 0.24], [0.3, 0.7, 0.7], [0.4, 0.3, 0.0], [1.0, 0.0, 0.0] ],
+                            'blue':    [ [0.0, 1.0, 1.0], [0.28, 0.0, 0.0], [1.0, 0.0, 0.0] ]
+                        }
+mars_colormap = LinearSegmentedColormap('mars_colormap', segmentdata=mars_colormap_dict, N=256)
 
+mars_red = mars_colormap(lum_img)
 
-
-
-
-
-
-
-# plt.imsave('mars_gray.png', mars_gray)
+plt.imsave('mars_red.png', mars_red)
 # plt.matshow(mars_gray)
 # plt.matshow(mars_red)
