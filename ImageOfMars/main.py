2369c75c1fa75acab0f39d661dd72ea8ccccb74e
btan-fluxus <56370618+btan-fluxus@users.noreply.github.com>
2021-08-29 17:15:07 -0700

    initial setup

---
+++ ImageOfMars/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+mars_gray = mpimg.imread('mars.png')
+
+
+
+
+
+
+
+
+
+
+# plt.matshow(mars_red)
