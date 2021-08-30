718272d97be4d9a1dab5f3fb1a2ca66288eab589
btan-fluxus <56370618+btan-fluxus@users.noreply.github.com>
2021-08-29 18:24:46 -0700

    add colormap

--- ImageOfMars/mars.py
+++ ImageOfMars/mars.py
@@ -3,16 +3,25 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-
-mars_gray = mpimg.imread('mars.png')
-
+import copy
 
 
+mars_gray = mpimg.imread('mars.png') # Shape (501, 1024, 3)
+lum_img = mars_gray[:, :, 0]
+# print(mars_gray.shape)
+# print(lum_img.shape)
+
+mars_colormap = {'red':     [ [0.0, 0.0, 0.0], [0.2, 0.0, 0.14], [0.75, 1.0, 1.0], [1.0, 0.7, 0.7] ],
+                 'green':   [ [0.0, 0.0, 0.14], [0.24, 0.14, 0.24], [0.3, 0.7, 0.7], [0.4, 0.3, 0.0], [1.0, 0.0, 0.0] ],
+                 'blue':    [ [0.0, 1.0, 1.0], [0.28, 0.0, 0.0], [1.0, 0.0, 0.0] ]}
 
 
 
 
 
 
+
+
+# plt.imsave('mars_gray.png', mars_gray)
 # plt.matshow(mars_gray)
 # plt.matshow(mars_red)
